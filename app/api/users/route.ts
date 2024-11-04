f5dd8f456da24c678d99865b7248c4ef8e3fd009
koleso8 <dev.koleso@gmail.com>
2024-11-04 11:27:54 +0200

    create api

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,18 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { prisma } from '../../../prisma/prisma-client';
 
-export function GET() {
-  return NextResponse.json({
-    users: ['user1', 'user2', 'user3'],
+export async function GET() {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
+}
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+
+  const user = await prisma.user.create({
+    data,
   });
+
+  return NextResponse.json(user);
 }
