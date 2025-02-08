50c328c73135ac9ccbe39b8cc4159eb06658637d
koleso8 <dev.koleso@gmail.com>
2025-02-08 13:46:59 +0200

    add neon db

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,9 +1,8 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse, NextRequest } from 'next/server';
 import { prisma } from '../../../prisma/prisma-client';
 
 export async function GET() {
   const users = await prisma.user.findMany();
-
   return NextResponse.json(users);
 }
 
