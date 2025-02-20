624d4bb913465f069b11a6d9c946d03c2007266f
koleso8 <dev.koleso@gmail.com>
2025-02-20 17:07:02 +0200

    drawer

--- shared/components/shared/CartDrawerItem.tsx
+++ shared/components/shared/CartDrawerItem.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { cn } from '@/shared/lib/utils';
-
-interface Props {
+import * as CartItem from './cart-item-details';
+import { CartItemProps } from './cart-item-details/cart-item-details.types';
+interface Props extends CartItemProps {
   className?: string;
 }
 
-export const CartDrawerItem: React.FC<Props> = ({ className }) => {
-  return <div className={cn('flex bg-white p-5 gap-6', className)}></div>;
+export const CartDrawerItem: React.FC<Props> = ({
+  id,
+  imageUrl,
+  name,
+  price,
+  quantity,
+  details,
+  className,
+}) => {
+  return (
+    <div className={cn('flex bg-white p-5 gap-6', className)}>
+      <CartItem.Image src={imageUrl} />
+      <div className="flex-1">
+        <CartItem.Info name={name} details={details} />
+      </div>
+    </div>
+  );
 };
--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -1,3 +1,4 @@
 export { getAvaliablePizzaSizes } from './getAvaliablePizzaSizes';
 export { getPizzaDetails } from './getPizzaDetails';
 export { calcTotalPizzaPrices } from './calcTotalPizzaPrices';
+export { getCartItemDetails } from './getCartItemDetails';
